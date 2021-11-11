b1ac35c48d1695460bda6c9cd3e552df7f66706e
Martin Edwin Schjødt Nielsen <MESN@ucl.dk>
2021-11-11 08:13:40 +0100

    initial commit

---
+++ WPFBROBRYG/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPFBROBRYG
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ButtonToClick.Click += buttonClick;
+
+        }
+        public void buttonClick(object sender, RoutedEventArgs e){
+
+            ButtonToClick.Background = new SolidColorBrush(Colors.Red);
+
+            LabelResult.Background = new SolidColorBrush(Colors.Red);
+            LabelResult.Content = "You Clicked the Button!";
+        }
+
+        
+    }
+}
